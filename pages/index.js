407a51a5c3b46e602e435968c9794d52a3cd68e0
Brennus Caio <brennuscaio@gmail.com>
2024-09-12 06:53:04 +0000

    Atualização da mensagem inicial.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Novamente aqui. Boa madrugada :) </h1>
+    return <h1> Novamente aqui. Boa madrugada 😌 </h1>
 }
-//aponta para o nextJs que esse é função que ele quer renderizar 
+//aponta para o nextJs que esse é função que ele quer renderizar.
 export default Home;
