4292a6dce97381d226dcdd1f0e1d70e350f72f36
Brennus Caio <brennuscaio@gmail.com>
2024-10-08 02:36:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Novamente aqui. Boa madrugada 😌 </h1>
+  return <h1> Novamente aqui. Boa madrugada 😌 </h1>;
 }
 //aponta para o nextJs que esse é função que ele quer renderizar.
 export default Home;
